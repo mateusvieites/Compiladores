0b6bfb92f56f94c34ffd1bfa6aeef9006cc1b87a
HardszVick <connorkenway753@gmail.com>
2020-10-05 20:33:11 -0300

    05/10

--- src/services/LexicalAnalyser.java
+++ src/services/LexicalAnalyser.java
@@ -8,13 +8,12 @@ import entities.Lexical;
 public class LexicalAnalyser {
 	Map<Integer,String> tokens = Lexical.getTokens();
 	
-	
 	public void analyse(String toBeAnalysed) {
 		Iterator it = tokens.entrySet().iterator(); 
 		while (it.hasNext()) { 
 			Map.Entry pairs = (Map.Entry)it.next(); 
 			if(pairs.getValue().equals("Label")){ 
-				System.out.println(pairs.getKey()); /* pega a chave da palavra X*/
+				System.out.println(pairs.getKey()); /* pegar a chave da palavra X*/
 			}else {
 				if(Character.isDigit(toBeAnalysed.charAt(0))){ /* começa com número */
 				
@@ -26,12 +25,18 @@ public class LexicalAnalyser {
 		}
 	}
 	
+	private static boolean isDigitOrIsLetter(char charTest) {
+		return Character.isDigit(charTest) || Character.isLetter(charTest);
+	}
+	
 	public void split(String text) {
 		String toBeAnalysed = "";
 		char c[] = text.toCharArray();
 		for(char aux : c) {
+			
 			System.out.println("Letra atual: " + aux);
-			if(Character.isDigit(aux) || Character.isLetter(aux)) {
+			
+			if(isDigitOrIsLetter(aux)) {
 				System.out.println("Aux é letra: " + aux);
 				toBeAnalysed += aux;
 			}else {
