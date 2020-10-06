31949870aacc479e16ee405d02362ef900610ddb
HardszVick <connorkenway753@gmail.com>
2020-10-05 21:08:47 -0300

    05/10

--- src/services/LexicalAnalyser.java
+++ src/services/LexicalAnalyser.java
@@ -6,57 +6,61 @@ import java.util.Map;
 import entities.Lexical;
 
 public class LexicalAnalyser {
-	Map<Integer,String> tokens = Lexical.getTokens();
-	
+	Map<Integer, String> tokens = Lexical.getTokens();
+
 	public void analyse(String toBeAnalysed) {
-		Iterator it = tokens.entrySet().iterator(); 
-		while (it.hasNext()) { 
-			Map.Entry pairs = (Map.Entry)it.next(); 
-			if(pairs.getValue().equals("Label")){ 
-				System.out.println(pairs.getKey()); /* pegar a chave da palavra X*/
-			}else {
-				if(Character.isDigit(toBeAnalysed.charAt(0))){ /* começa com número */
-				
-				}else {
+		Iterator it = tokens.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pairs = (Map.Entry) it.next();
+			if (pairs.getValue().equals("Label")) {
+				System.out.println(pairs.getKey()); /* pegar a chave da palavra X */
+			} else {
+				if (Character.isDigit(toBeAnalysed.charAt(0))) { /* começa com número */
+
+				} else {
 					/* adicionar o elemento como identificador */
 				}
-					
+
 			}
 		}
 	}
-	
+
 	private static boolean isDigitOrIsLetter(char charTest) {
 		return Character.isDigit(charTest) || Character.isLetter(charTest);
 	}
-	
+
 	public void split(String text) {
 		String toBeAnalysed = "";
 		char c[] = text.toCharArray();
-		for(char aux : c) {
-			
+		for (char aux : c) {
+
 			System.out.println("Letra atual: " + aux);
-			
-			if(isDigitOrIsLetter(aux)) {
+
+			if (isDigitOrIsLetter(aux)) {
+				if (!isDigitOrIsLetter(toBeAnalysed.charAt(0))) {
+					// #ToDo analisar
+					toBeAnalysed = "";
+				}
 				System.out.println("Aux é letra: " + aux);
 				toBeAnalysed += aux;
-			}else {
-				if(!(toBeAnalysed.equals(""))) {
+			} else {
+				if (!(toBeAnalysed.equals(""))) {
 					System.out.println("Aux diferente: " + aux);
-					//#ToDo analisar
+					// #ToDo analisar
 					toBeAnalysed = "";
-					if(aux != ' ') {
+					if (aux != ' ') {
 						System.out.println("Aqui o:" + aux);
 						toBeAnalysed += aux;
 					}
-				}else {
-					if(Character.getType(aux) != 12) {
+				} else {
+					if (Character.getType(aux) != 12) {
 						toBeAnalysed += aux;
 					}
 				}
-					
+
 			}
-			
+
 		}
-		
+
 	}
 }
